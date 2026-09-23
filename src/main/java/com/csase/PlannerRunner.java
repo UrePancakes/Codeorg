904a4c77c8835859cd884e355af3241bedd32fd0
UrePancakes <Amaure.Rosado@stu.lok12.org>
2026-09-23 10:02:01 -0400

    Fix to student.java

--- src/main/java/com/csase/PlannerRunner.java
+++ src/main/java/com/csase/PlannerRunner.java
@@ -4,10 +4,10 @@ public class PlannerRunner {
     public static void main(String[] args) {
 
         // Create a new student
-        Student jada = new Student();
+        Student student = new Student();
 
         // Gets the student's choice until the student exits
-        jada.getChoice();
+        student.getChoice();
 
     }
 }
